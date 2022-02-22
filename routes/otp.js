ca0c94d3033d7320e5bc266e55749a6e8d40f84e
Mrityunjay <mrityunjayvyasmhl@gmail.com>
2022-02-22 15:44:12 +0530

    CORS Applied

--- routes/otp.js
+++ routes/otp.js
@@ -2,21 +2,22 @@ const express = require("express");
 const router = express.Router();
 const nodemailer = require('nodemailer');
 
-
 const User = require("../models/user");
 
 
+// Sending OTP route
 router.get("/:apiKey/:reciverMail", (req, res) => {
-  const apiKey = req.params.apiKey;
-  const reciverMail = req.params.reciverMail;
+  const apiKey = req.params.apiKey;//API Key Provided by caller
+  const reciverMail = req.params.reciverMail;//reciver mailId provided by caller
 
  User.findOne({apiKey}, async (err, foundUser) => {
     if(foundUser){//checking if the API KEy is Valid
 
       if(foundUser.noOfCalls > 0){//Checking if your have api calls left
 
-        let otp = Math.floor(Math.random() * 100000);
+        let otp = Math.floor(Math.random() * 100000);//Generating a randon number
 
+        //html mail template
         const output = `
           <div style="text-align: center">
             <h3>OTP: ${otp}</h3>
@@ -50,21 +51,26 @@ router.get("/:apiKey/:reciverMail", (req, res) => {
         await transporter.sendMail(mailOptions, function(error, info) {
           if (error) {
             console.log(error);
-            return error;
+            res.send(error);
           } else {
             console.log('Email sent: ' + info.response);
 
+            //if the call is Successful, decrementing the number of calls left with user
             foundUser.noOfCalls--;
             foundUser.save();
+
+            //returning the otp as json object
             res.json({otp});
           }
         });
 
       }else{
+        //If user didnt have credits left
         res.send("You have exhausted api calls credit, please buy some more!!!");
       }
 
     }else{
+      //API Key is not valid condition
       res.status(400).send("Invalid API Key");
     }
   });
