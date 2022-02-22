60e9b4e6c10bbd3a036bfd436aa81726db5a528e
Mrityunjay <mrityunjayvyasmhl@gmail.com>
2022-02-22 18:02:18 +0530

    callback Updated

--- models/user.js
+++ models/user.js
@@ -53,7 +53,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/mailS",
+    callbackURL: "https://mailserviceotp.herokuapp.com/auth/google/mailS",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {//callback with profile details from google
@@ -73,7 +73,7 @@ passport.use(new GoogleStrategy({
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/github/mailS"
+    callbackURL: "https://mailserviceotp.herokuapp.com/auth/github/mailS"
   },
   function(accessToken, refreshToken, profile, done) {//callback with profile details from github
     User.findOrCreate({ username: profile.username, githubId: profile.id }, function (err, user) {
